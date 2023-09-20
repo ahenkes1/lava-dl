933b5036fdd31d0f8be3569036b3856a74391f0d
Alexander Henkes <alexanderhenkes1@gmail.com>
2023-09-20 11:59:20 +0200

    Added 'cuba_potspk.py' as a simple copy of 'cuba.py'.

--- src/lava/lib/dl/slayer/block/__init__.py
+++ src/lava/lib/dl/slayer/block/__init__.py
@@ -1,12 +1,13 @@
 # Copyright (C) 2022 Intel Corporation
 # SPDX-License-Identifier:  BSD-3-Clause
 
-from . import cuba, rf, rf_iz, alif, adrf, adrf_iz, sigma_delta
+from . import cuba, rf, rf_iz, alif, adrf, adrf_iz, sigma_delta, cuba_potspk
 
 __all__ = [
     'cuba',
     'rf', 'rf_iz',
     'alif',
     'adrf', 'adrf_iz',
-    'sigma_delta'
+    'sigma_delta',
+    'cuba_potspk',
 ]
